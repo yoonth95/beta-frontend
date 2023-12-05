059692de0ef5b0ed73e1c5fc5b5e32c7c81bcebd
윤태현 <yoonth0919@gmail.com>
2023-12-05 14:01:34 +0900

    Design: 로그인 페이지 디자인 수정 (#3)

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Button from "@/components/common/Button/Button";
-import InputField from "@/components/common/InputField/InputField";
+import { SignForm, Button, InputField } from "@/components/common";
 import styles from "./LoginPage.module.css";
 
 const LoginPage = () => {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
-  const [userType, setUserType] = useState("User");
+  const [userType, setUserType] = useState<"User" | "Admin">("User");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -16,35 +15,17 @@ const LoginPage = () => {
 
   return (
     <main className={styles["login-main"]}>
-      <section className={styles["login-section"]}>
-        <div className={styles["login-section-buttons"]}>
-          <button
-            type="button"
-            className={`${styles["login-section-button"]} ${userType === "User" ? styles["active"] : ""}`}
-            onClick={() => setUserType("User")}
-          >
-            일반회원 로그인
-          </button>
-          <button
-            type="button"
-            className={`${styles["login-section-button"]} ${userType === "Admin" ? styles["active"] : ""}`}
-            onClick={() => setUserType("Admin")}
-          >
-            관리자 로그인
-          </button>
-        </div>
+      <SignForm userType={userType} setUserType={setUserType}>
         <form onSubmit={handleSubmit} className={styles["login-section-form"]}>
-          <InputField children="아이디" type="text" placeholder="아이디를 입력해주세요." value={id} onChange={(e) => setId(e.target.value)} />
-          <InputField
-            children="비밀번호"
-            type="password"
-            placeholder="비밀번호를 입력해주세요."
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <InputField type="text" placeholder="아이디를 입력해주세요." value={id} onChange={(e) => setId(e.target.value)}>
+            아이디
+          </InputField>
+          <InputField type="password" placeholder="비밀번호를 입력해주세요." value={password} onChange={(e) => setPassword(e.target.value)}>
+            비밀번호
+          </InputField>
           <Button children="로그인" type="submit" />
         </form>
-      </section>
+      </SignForm>
       <section className={styles["login-link"]}>
         <Link to="/signup">회원가입</Link>
         <span className={styles["login-link-span"]}>|</span>
