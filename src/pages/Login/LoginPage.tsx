8db3be0ccb7987dd90584e07028c205c9698496d
윤태현 <yoonth0919@gmail.com>
2023-12-05 10:21:31 +0900

    Design: 로그인 페이지 마크업 및 디자인 (#3)

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "@/components/common/Button/Button";
+import InputField from "@/components/common/InputField/InputField";
+import styles from "./LoginPage.module.css";
 
 const LoginPage = () => {
-  return <div></div>;
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [userType, setUserType] = useState("User");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(id, password, userType);
+  };
+
+  return (
+    <main className={styles["login-main"]}>
+      <section className={styles["login-section"]}>
+        <div className={styles["login-section-buttons"]}>
+          <button
+            type="button"
+            className={`${styles["login-section-button"]} ${userType === "User" ? styles["active"] : ""}`}
+            onClick={() => setUserType("User")}
+          >
+            일반회원 로그인
+          </button>
+          <button
+            type="button"
+            className={`${styles["login-section-button"]} ${userType === "Admin" ? styles["active"] : ""}`}
+            onClick={() => setUserType("Admin")}
+          >
+            관리자 로그인
+          </button>
+        </div>
+        <form onSubmit={handleSubmit} className={styles["login-section-form"]}>
+          <InputField children="아이디" type="text" placeholder="아이디를 입력해주세요." value={id} onChange={(e) => setId(e.target.value)} />
+          <InputField
+            children="비밀번호"
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button children="로그인" type="submit" />
+        </form>
+      </section>
+      <section className={styles["login-link"]}>
+        <Link to="/signup">회원가입</Link>
+        <span className={styles["login-link-span"]}>|</span>
+        <Link to="/findPW">비밀번호 찾기</Link>
+      </section>
+    </main>
+  );
 };
 
 export default LoginPage;
