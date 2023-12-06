6f57c1df71e154cdd0f4d7ccad740c07e402e6ab
ShinEun9 <ses2201@naver.com>
2023-12-06 13:22:24 +0900

    Design: MainPage 마크업 및 스타일(#2)

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Header, NavBar } from "@/components/layouts";
+import { Banner, StorySection, CalendarSection } from "@/components/main";
+import styles from "./MainPage.module.css";
 
 const MainPage = () => {
-  return <div>dsadsadas</div>;
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <main className={styles.main}>
+        <Banner />
+        <StorySection />
+        <CalendarSection />
+      </main>
+    </>
+  );
 };
 
 export default MainPage;
