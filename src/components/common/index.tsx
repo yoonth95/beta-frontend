e6898c4c07d554a807758b2b9107f7c9b4813e85
윤태현 <yoonth0919@gmail.com>
2023-12-06 11:26:04 +0900

    Design: 공통 컴포넌트 다중 입력 필드 마크업 및 디자인 (#4)

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -3,5 +3,6 @@ import InputField from "./InputField/InputField";
 import SignForm from "./SignForm/SignForm";
 import SelectBox from "./SelectBox/SelectBox";
 import Carousel from "./Carousel/Carousel";
+import InputFieldGroup from "./InputFieldGroup/InputFieldGroup";
 
-export { Button, InputField, SignForm, SelectBox, Carousel };
+export { Button, InputField, SignForm, SelectBox, Carousel, InputFieldGroup };
