859c87726d09e421a022189e40204186dc2c26f8
ebokyung <dlqhrud303@naver.com>
2023-12-06 15:35:45 +0900

    Design: Detail 페이지 main 기본 마크업 (#1)

--- src/pages/Detail/DetailPage.tsx
+++ src/pages/Detail/DetailPage.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Carousel } from "@/components/common";
+import { LikeButton, SubMenu } from "@/components/detail";
+import { Outlet, useLocation } from "react-router-dom";
+
+const item = {
+  id: 1,
+  organizer: "고려대학교 미술학과",
+  imgSrc: ["/concert-img.jpg", "/concert-img2.jpg", "/concert-img3.jpg", "/card-img.png"],
+  title: "고려대 전시회에 놀러오세요",
+  location: "고려대학교 박물관 지하 1층 (백주년기념 삼성관) 기획 전시실 Ⅱ",
+  date: "2023.11.23 - 2023.11.29",
+  tags: ["abc", "def", "ghi", "jkl", "mno"],
+  position: {
+    lat: 37.5069494959122,
+    lng: 127.055596615858,
+  },
+};
+
+const submenuList = [
+  { pathname: "info", text: "정보" },
+  { pathname: "review", text: "후기/방명록" },
+];
 
 const DetailPage = () => {
-  return <div></div>;
+  const location = useLocation();
+  return (
+    <main>
+      <Carousel imgs={item.imgSrc} />
+      <LikeButton active={false} />
+      <section>
+        <div style={{ display: "flex" }}>
+          {submenuList.map((menu) => (
+            <SubMenu key={menu.text} selected={location.pathname.includes(menu.pathname)} url={`./${menu.pathname}`}>
+              {menu.text}
+            </SubMenu>
+          ))}
+        </div>
+        <Outlet context={{ item }} />
+      </section>
+    </main>
+  );
 };
 
 export default DetailPage;
