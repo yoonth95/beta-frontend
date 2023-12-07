8371304ca3cd05737f421b6f964ae864048a31cb
ebokyung <dlqhrud303@naver.com>
2023-12-07 14:57:30 +0900

    Feat: Modal 컴포넌트 뒤 스크롤 막기, 닫기 기능 구현 (#13)

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useModalStore } from "@/stores/useModalStore";
 import styles from "./Modal.module.css";
 
 interface PropsType {
@@ -6,10 +7,24 @@ interface PropsType {
 }
 
 const Modal: React.FC<PropsType> = ({ children }) => {
+  const { setIsOpenModal } = useModalStore();
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
   return (
     <article className={styles["modal-background"]}>
       <div className={styles["modal"]}>
-        <button className={styles["modal__close"]}>x</button>
+        <button onClick={closeModal} className={styles["modal__close"]}>
+          x
+        </button>
         {children}
       </div>
     </article>
