8ead83b626a8757bf541b8218b40b65bf8c24def
ShinEun9 <ses2201@naver.com>
2023-12-08 16:52:16 +0900

    Feat: 스토리 섹션 스타일 수정 및 모달 기능 추가(#2)

--- src/components/main/StorySection/StorySection.tsx
+++ src/components/main/StorySection/StorySection.tsx
@@ -1,27 +1,50 @@
-import { Story } from "@/components/main";
-import { Carousel } from "@/components/common";
+import { useState } from "react";
+import { Carousel, Modal } from "@/components/common";
+import { Story, StoryUploadModal, StoryViewModal } from "@/components/main";
+import { useModalStore } from "@/stores/useModalStore";
 import styles from "./StorySection.module.css";
 
 const StorySection = () => {
   const items = { imgSrc: "/public/story-img.jpg", title: "#멋지다신은수, #졸업축하해" };
+  const [modalType, setModalType] = useState("");
+  const { isOpenModal, setIsOpenModal } = useModalStore();
 
+  const handleClickUploadBtn = () => {
+    setIsOpenModal(true);
+    setModalType("upload");
+  };
+  const handleClickMoreBtn = () => {
+    setIsOpenModal(true);
+    setModalType("more");
+  };
   return (
-    <section className={styles["section"]}>
-      <div className={styles["header"]}>
-        <h2 className={styles["header__title"]}>스토리</h2>
-        <button type="button" className={styles["header__story-btn"]}>
-          <span className="a11y-hidden">스토리 추가버튼</span>
-        </button>
-      </div>
+    <>
+      <section className={styles["section"]}>
+        <div className={styles["header"]}>
+          <h2 className={styles["header__title"]}>스토리</h2>
+          <button type="button" className={styles["story-add-btn"]} onClick={handleClickUploadBtn}>
+            <span className="a11y-hidden">스토리 추가버튼</span>
+          </button>
+          <button className={styles["story-more-btn"]} type="button" onClick={handleClickMoreBtn}>
+            더보기
+          </button>
+        </div>
 
-      <Carousel index={1}>
-        {Array(5)
-          .fill(items)
-          .map(({ imgSrc, title }) => (
-            <Story key={title} title={title} imgSrc={imgSrc} />
-          ))}
-      </Carousel>
-    </section>
+        <Carousel index={1}>
+          {Array(5)
+            .fill(items)
+            .map(({ imgSrc, title }) => (
+              <Story key={title} title={title} imgSrc={imgSrc} />
+            ))}
+        </Carousel>
+        {isOpenModal && (
+          <Modal>
+            {modalType === "upload" && <StoryUploadModal />}
+            {modalType === "more" && <StoryViewModal />}
+          </Modal>
+        )}
+      </section>
+    </>
   );
 };
 
---
+++ src/components/main/StoryViewModal/StoryViewModal.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./StoryViewModal.module.css";
+
+const StoryViewModal = () => {
+  return <div></div>;
+};
+
+export default StoryViewModal;
