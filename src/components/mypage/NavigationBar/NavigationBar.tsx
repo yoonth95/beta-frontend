c3b60842c03b0cf3b81dbe75962fd71638045e83
윤태현 <yoonth0919@gmail.com>
2023-12-08 14:47:23 +0900

    Refactor: 마이페이지 네비게이션 바 useSearchParams 사용 및 코드 최적화 (#15)

--- src/components/mypage/NavigationBar/NavigationBar.tsx
+++ src/components/mypage/NavigationBar/NavigationBar.tsx
@@ -1,33 +1,31 @@
 import { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useSearchParams } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "./NavigationBar.module.css";
 
 const cx = classNames.bind(styles);
 
 const userNavList = [
-  { type: "info", pathname: "/mypage?tab=profile", text: "프로필" },
-  { type: "manage", pathname: "/mypage?tab=like", text: "좋아요" },
-  { type: "manage", pathname: "/mypage?tab=review", text: "후기" },
-  { type: "manage", pathname: "/mypage?tab=story", text: "스토리" },
-  { type: "manage", pathname: "/mypage?tab=reservation", text: "예매내역" },
+  { type: "info", pathname: "profile", text: "프로필" },
+  { type: "manage", pathname: "like", text: "좋아요" },
+  { type: "manage", pathname: "review", text: "후기" },
+  { type: "manage", pathname: "story", text: "스토리" },
+  { type: "manage", pathname: "reservation", text: "예매내역" },
 ];
 
 const adminNavList = [
-  { type: "info", pathname: "/mypage?tab=profile", text: "프로필" },
-  { type: "manage", pathname: "/mypage?tab=post", text: "게시글" },
-  { type: "manage", pathname: "/mypage?tab=reservation-manage", text: "예매관리" },
+  { type: "info", pathname: "profile", text: "프로필" },
+  { type: "manage", pathname: "post", text: "게시글" },
+  { type: "manage", pathname: "reservation-manage", text: "예매관리" },
 ];
 
 const Navigation = () => {
   const [isAdmin, setIsAdmin] = useState(false); // 전역 상태로 관리할 예정
-  const location = useLocation();
-  const nowLocation = location.search.split("=")[1];
+  const [searchParams] = useSearchParams();
+  const urlPathname = searchParams.get("tab");
 
-  const checkIsActive = (pathname: string) => {
-    if (nowLocation === pathname.split("=")[1] || (location.search === "" && pathname === "/mypage?tab=profile")) {
-      return true;
-    }
+  const checkUrl = (urlPathname: string | null, pathname: string) => {
+    return urlPathname ? pathname === urlPathname : pathname === "profile";
   };
 
   const navList = isAdmin ? adminNavList : userNavList;
@@ -39,7 +37,7 @@ const Navigation = () => {
         {navList
           .filter((item) => item.type === "info")
           .map((item) => (
-            <NavLink key={item.pathname} to={item.pathname} className={cx(checkIsActive(item.pathname) && "active")}>
+            <NavLink key={item.pathname} to={`?tab=${item.pathname}`} className={cx(checkUrl(urlPathname, item.pathname) && "active")}>
               {item.text}
             </NavLink>
           ))}
@@ -47,7 +45,7 @@ const Navigation = () => {
         {navList
           .filter((item) => item.type === "manage")
           .map((item) => (
-            <NavLink key={item.pathname} to={item.pathname} className={cx(checkIsActive(item.pathname) && "active")}>
+            <NavLink key={item.pathname} to={`?tab=${item.pathname}`} className={cx(checkUrl(urlPathname, item.pathname) && "active")}>
               {item.text}
             </NavLink>
           ))}
