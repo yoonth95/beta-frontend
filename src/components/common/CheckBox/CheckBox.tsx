0b0a6bbe0e4fa84cf91900241af4b196086d4954
ebokyung <dlqhrud303@naver.com>
2023-12-09 23:46:06 +0900

    Feat: CheckBox 컴포넌트에 name, checked prop 추가 (#4)

--- src/components/common/CheckBox/CheckBox.tsx
+++ src/components/common/CheckBox/CheckBox.tsx
@@ -4,12 +4,14 @@ import styles from "./CheckBox.module.css";
 interface PropsType {
   children: React.ReactNode;
   inputId: string;
+  name?: string;
+  checked: boolean;
 }
 
-const CheckBox: React.FC<PropsType> = ({ children, inputId }) => {
+const CheckBox: React.FC<PropsType> = ({ children, inputId, name, checked }) => {
   return (
     <fieldset className={styles["fieldset"]}>
-      <input className={styles["fieldset__checkbox"]} type="checkbox" id={inputId} />
+      <input className={styles["fieldset__checkbox"]} type="checkbox" id={inputId} name={name} checked={checked} />
       <label className={styles["fieldset__label"]} htmlFor={inputId}>
         {children}
       </label>
