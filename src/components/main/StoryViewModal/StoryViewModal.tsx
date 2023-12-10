0b15e3508172cd39369f3d034dec6dde78050eca
ShinEun9 <ses2201@naver.com>
2023-12-10 11:30:04 +0900

    Design: 스토리 모달 마크업 및 스타일(#2)

--- src/components/main/StoryViewModal/StoryViewModal.tsx
+++ src/components/main/StoryViewModal/StoryViewModal.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import { Carousel } from "@/components/common";
 import styles from "./StoryViewModal.module.css";
+import Color from "color-thief-react";
+
+const items = [
+  { imgSrc: "/public/card-image.png", tags: "#멋지다신은수, #졸업축하해" },
+  { imgSrc: "/public/story-img.jpg", tags: "#멋지다신은수, #졸업축하해#멋지다신은수" },
+];
 
 const StoryViewModal = () => {
-  return <div></div>;
+  return (
+    <div className={styles["modal"]}>
+      <Carousel index={2}>
+        {items.map(({ imgSrc, tags }) => {
+          return (
+            <Color src={imgSrc} format="hex">
+              {({ data: backgroundColor }) => (
+                <article className={styles.card} style={{ backgroundColor }}>
+                  <strong className={styles["card__nickname"]}>@유저닉네임</strong>
+                  <div className={styles["card__img-wrapper"]}>
+                    <img className={styles["card__img"]} src={imgSrc} />
+                  </div>
+                  <div className={styles["card__tags"]}>{tags}</div>
+                </article>
+              )}
+            </Color>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
 };
 
 export default StoryViewModal;
