b0fbd9d7ed4cf09e2befc2e35b620707919599c2
ebokyung <dlqhrud303@naver.com>
2023-12-10 20:00:33 +0900

    Feat: RadioButtonGroup 컴포넌트에 name, defaultValue, onChange, flexDirectionColumn prop 추가 (#4)

--- src/components/common/RadioButtonGroup/RadioButtonGroup.tsx
+++ src/components/common/RadioButtonGroup/RadioButtonGroup.tsx
@@ -1,22 +1,31 @@
 import React, { useState } from "react";
 import styles from "./RadioButtonGroup.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 interface PropsType {
   radioList: string[];
+  name: string;
+  defaultValue?: string;
+  onChange: React.ChangeEventHandler;
+  flexDirectionColumn?: boolean;
 }
 
-const RadioButtonGroup: React.FC<PropsType> = ({ radioList }) => {
-  const [selectedOption, setSelectedOption] = useState(radioList[0]);
+const RadioButtonGroup: React.FC<PropsType> = ({ radioList, name, defaultValue, onChange, flexDirectionColumn = false }) => {
+  console.log(defaultValue);
+  const [selectedOption, setSelectedOption] = useState(defaultValue || radioList[0]);
 
   const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedOption(e.target.value);
+    onChange(e);
   };
 
   return (
-    <fieldset className={styles["fieldset"]}>
+    <fieldset className={cx("fieldset", flexDirectionColumn && "row")}>
       {radioList.map((item) => (
         <label key={item}>
-          <input type="radio" name="round" value={item} checked={selectedOption === item} onChange={handleOptionChange} />
+          <input type="radio" name={name} value={item} checked={selectedOption === item} onChange={handleOptionChange} />
           <span>{item}</span>
         </label>
       ))}
