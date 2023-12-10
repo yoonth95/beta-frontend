a633bf34b8717c3c6159d43800b11280d9dd4229
ShinEun9 <ses2201@naver.com>
2023-12-10 11:28:46 +0900

    Design: 스토리 업로드 폼 모달 마크업 및 스타일 수정(#2)

--- src/components/main/StoryUploadModal/StoryUploadModal.tsx
+++ src/components/main/StoryUploadModal/StoryUploadModal.tsx
@@ -2,24 +2,26 @@ import { useState } from "react";
 import { Button, TagInput } from "@/components/common";
 import styles from "./StoryUploadModal.module.css";
 
-const StoryUpload = () => {
-  const [imgFile, setImgFile] = useState<File | null>(null);
+const StoryUploadModal = () => {
+  const [imgPreview, setImgPreview] = useState<string>("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setImgFile(e.target.files?.[0] as File | null);
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      const imgSrc = URL.createObjectURL(selectedFile);
+      setImgPreview(imgSrc);
+    }
   };
 
   return (
     <div className={styles["modal"]}>
-      <h1 className={styles["modal__title"]}>스토리 업로드</h1>
-      <div className={styles["modal__img-container"]} style={{ backgroundImage: imgFile ? `url(${URL.createObjectURL(imgFile)})` : "" }}>
-        <label className={styles["img-upload"]} htmlFor="chooseFile"></label>
-        <input style={{ display: "none" }} type="file" id="chooseFile" name="chooseFile" accept="image/*" onChange={handleChange} />
-      </div>
+      <label className={styles["img-upload"]} htmlFor="img" style={{ backgroundImage: `url(${imgPreview})` }}>
+        <input type="file" id="img" accept="image/*" onChange={handleChange} />
+      </label>
       <TagInput />
       <Button borderRadius={"16px"}>업로드하기</Button>
     </div>
   );
 };
 
-export default StoryUpload;
+export default StoryUploadModal;
