c4d21e3dce3886e88075d0d646e9e1209c550e3d
ebokyung <dlqhrud303@naver.com>
2023-12-10 15:05:08 +0900

    Refactor: CheckBox 컴포넌트에 onChange prop 추가 (#4)

--- src/components/common/CheckBox/CheckBox.tsx
+++ src/components/common/CheckBox/CheckBox.tsx
@@ -6,12 +6,13 @@ interface PropsType {
   inputId: string;
   name?: string;
   checked: boolean;
+  onChange: React.ChangeEventHandler;
 }
 
-const CheckBox: React.FC<PropsType> = ({ children, inputId, name, checked }) => {
+const CheckBox: React.FC<PropsType> = ({ children, inputId, name, checked, onChange }) => {
   return (
     <fieldset className={styles["fieldset"]}>
-      <input className={styles["fieldset__checkbox"]} type="checkbox" id={inputId} name={name} checked={checked} />
+      <input className={styles["fieldset__checkbox"]} type="checkbox" id={inputId} name={name} checked={checked} onChange={onChange} />
       <label className={styles["fieldset__label"]} htmlFor={inputId}>
         {children}
       </label>
