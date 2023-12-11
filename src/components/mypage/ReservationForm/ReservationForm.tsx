6990e6ba65a23cb8f3cd4f9469bb12313724c473
ebokyung <dlqhrud303@naver.com>
2023-12-12 00:30:58 +0900

    Design: MyPage ReservationForm 스타일 추가 (#15)

--- src/components/mypage/ReservationForm/ReservationForm.tsx
+++ src/components/mypage/ReservationForm/ReservationForm.tsx
@@ -23,21 +23,21 @@ const ReservationForm = () => {
     // roundList 추가
   };
   return (
-    <>
+    <div className={styles["reservation-form-wrapper"]}>
       <InputField type="text">가격</InputField>
       <InputField type="text">총 수용 가능 인원</InputField>
       <section className={styles["round-list"]}>
-        <h4>회차</h4>
+        <h4 className={styles["title"]}>회차</h4>
         {roundList.map((round) => (
           <article className={styles["round-item"]}>
             <div className={styles["round-item__box"]}>
               <div className={styles["round-item__box-row"]}>
-                <span>날짜</span>
-                {round.date}
+                <span className={styles["round-item__title"]}>날짜</span>
+                <span className={styles["round-item__content"]}>{round.date}</span>
               </div>
               <div className={styles["round-item__box-row"]}>
-                <span>시간</span>
-                {round.time}
+                <span className={styles["round-item__title"]}>시간</span>
+                <span className={styles["round-item__content"]}>{round.time}</span>
               </div>
             </div>
             <button>x</button>
@@ -45,19 +45,21 @@ const ReservationForm = () => {
         ))}
 
         <article className={styles["round-item-add"]}>
-          <div>
-            <span>날짜 및 시간</span>
+          <div className={styles["round-item-add__dateTime"]}>
+            <span className={styles["round-item__title"]}>날짜 및 시간</span>
             <DatePeriodPicker type="dateWithTime" onChange={handleDateTimeInput} />
           </div>
-          <button onClick={handleRoundAdd}>회차 추가</button>
+          <button className={styles["round-item-add__btn"]} onClick={handleRoundAdd}>
+            회차 추가
+          </button>
         </article>
       </section>
 
       <section>
-        <h4>유의사항</h4>
-        {/* <Editor /> */}
+        <h4 className={styles["title"]}>유의사항</h4>
+        <Editor />
       </section>
-    </>
+    </div>
   );
 };
 
