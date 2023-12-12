865cb998bbf85a7412632466a9864d16638ceab8
ebokyung <dlqhrud303@naver.com>
2023-12-12 21:18:37 +0900

    Refactor: useDetailDataStore에 ShowItem 타입 적용 및 임시 기본값 지우기 (#1)

--- src/stores/useDetailDataStore.ts
+++ src/stores/useDetailDataStore.ts
@@ -1,40 +1,13 @@
+import { ShowItem } from "@/types/ShowItem";
 import { create } from "zustand";
 
-export interface Item {
-  id: number;
-  organizer: string;
-  imgSrc: string[];
-  title: string;
-  location: string;
-  date: string;
-  tags: string[];
-  position: Position;
-}
-
-export interface Position {
-  lat: number;
-  lng: number;
-}
-
 interface useDetailDataStoreType {
-  item: Item;
-  setItemData: (item: Item) => void;
+  item: ShowItem | null;
+  setItemData: (item: ShowItem) => void;
 }
 
 export const useDetailDataStore = create<useDetailDataStoreType>((set) => ({
-  item: {
-    id: 1,
-    organizer: "서울대학교 미술학과",
-    imgSrc: ["/concert-img.jpg", "/concert-img2.jpg", "/concert-img3.jpg", "/card-image.png"],
-    title: "고려대 전시회에 놀러오세요",
-    location: "고려대학교 박물관 지하 1층 (백주년기념 삼성관) 기획 전시실 Ⅱ",
-    date: "2023.11.23 - 2023.11.29",
-    tags: ["abc", "def", "ghi", "jkl", "mno"],
-    position: {
-      lat: 37.5069494959122,
-      lng: 127.055596615858,
-    },
-  },
+  item: null,
   setItemData: (item) =>
     set(() => ({
       item,
