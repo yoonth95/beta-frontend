357eefd48b22bf9f1175f85a002d0eab017b797b
ShinEun9 <ses2201@naver.com>
2023-12-14 12:57:18 +0900

    Feat: 필터값이 바뀔 때마다 Exhibition정보 fetch할 수 있도록 구현(#20)

--- src/components/mainExhibition/ExhibitionListSection/ExhibitionListSection.tsx
+++ src/components/mainExhibition/ExhibitionListSection/ExhibitionListSection.tsx
@@ -1,27 +1,32 @@
-import React from "react";
-import styles from "./ExhibitionListSection.module.css";
+import { useQuery } from "@tanstack/react-query";
 import { BasicCard } from "@/components/common";
+import { getShows } from "@/apis";
+import { ShowFilterRequestType } from "@/types/";
+import styles from "./ExhibitionListSection.module.css";
 
-const item = {
-  id: "1",
-  imgSrc: "/public/card-image.png",
-  title: "서울대 전시회에 놀러오세요",
-  location: "고려대학교 박물관 지하 1층 (백주년기념 삼성관) 기획 전시실 Ⅱ",
-  date: "2023.11.23 - 2023.11.29",
-};
+interface PropsType {
+  filterRequest: ShowFilterRequestType;
+}
 
-const ExhibitionListSection = () => {
+const ExhibitionListSection: React.FC<PropsType> = ({ filterRequest }) => {
+  const { start_date, end_date, location, progress } = filterRequest;
+  const { data, status, error } = useQuery({
+    queryKey: ["exhibitionData", filterRequest],
+    queryFn: async () => await getShows("exhibition", start_date, end_date, location, progress),
+  });
+
+  if (status === "pending") return <>loading...</>;
+  if (status === "error") return <>{error.message}</>;
+
   return (
     <section className={styles["section"]}>
       <h2 className="a11y-hidden">전시 리스트</h2>
       <ul className={styles["exhibition-list"]}>
-        {Array(9)
-          .fill(0)
-          .map(() => (
-            <li>
-              <BasicCard item={item} />
-            </li>
-          ))}
+        {data.map((item) => (
+          <li>
+            <BasicCard item={item} />
+          </li>
+        ))}
       </ul>
     </section>
   );
--- src/pages/MainExhibition/MainExhibitionPage.tsx
+++ src/pages/MainExhibition/MainExhibitionPage.tsx
@@ -1,15 +1,24 @@
-import React from "react";
-import { NavBar } from "@/components/layouts";
+import { useState } from "react";
 import { Filters } from "@/components/common";
+import { NavBar } from "@/components/layouts";
 import { ExhibitionListSection } from "@/components/mainExhibition/";
+import getTodayStringDate from "@/utils/getTodayStringDate";
 
 const MainExhibitionPage = () => {
+  const { todayString } = getTodayStringDate();
+  const [filterRequest, setFilterRequest] = useState({
+    start_date: todayString,
+    end_date: todayString,
+    location: "all",
+    progress: "all",
+  });
+
   return (
     <>
       <NavBar />
       <main>
-        <Filters />
-        <ExhibitionListSection />
+        <Filters filterRequest={filterRequest} setFilterRequest={setFilterRequest} />
+        <ExhibitionListSection filterRequest={filterRequest} />
       </main>
     </>
   );
