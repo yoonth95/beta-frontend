da99b77e19814e2f8733eaa3448e84ec63de87ff
ebokyung <dlqhrud303@naver.com>
2023-12-14 22:31:57 +0900

    Fix: key 추가

--- src/components/mypage/ReservationForm/ReservationForm.tsx
+++ src/components/mypage/ReservationForm/ReservationForm.tsx
@@ -3,6 +3,9 @@ import { DatePicker, Editor, InputField } from "@/components/common";
 import styles from "./ReservationForm.module.css";
 
 const ReservationForm = () => {
+  const onChange = () => {
+    //
+  };
   const [roundList, setRoundList] = useState([
     { date: "2023-12-08", time: "오후 1시" },
     { date: "2023-12-08", time: "오후 1시" },
@@ -24,12 +27,16 @@ const ReservationForm = () => {
   };
   return (
     <div className={styles["reservation-form-wrapper"]}>
-      <InputField type="text">가격</InputField>
-      <InputField type="text">총 수용 가능 인원</InputField>
+      <InputField type="text" value={""} onChange={onChange}>
+        가격
+      </InputField>
+      <InputField type="text" value={""} onChange={onChange}>
+        총 수용 가능 인원
+      </InputField>
       <section className={styles["round-list"]}>
         <h4 className={styles["title"]}>회차</h4>
-        {roundList.map((round) => (
-          <article className={styles["round-item"]}>
+        {roundList.map((round, index) => (
+          <article key={index} className={styles["round-item"]}>
             <div className={styles["round-item__box"]}>
               <div className={styles["round-item__box-row"]}>
                 <span className={styles["round-item__title"]}>날짜</span>
