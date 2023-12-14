c29b497c05cdd5f43564fd56ef79fdcd8d8e7399
ShinEun9 <ses2201@naver.com>
2023-12-14 12:56:11 +0900

    Feat: 필터값이 바뀔 때마다 Concert정보 fetch할 수 있도록 구현(#20)

--- src/pages/MainConcert/MainConcertPage.tsx
+++ src/pages/MainConcert/MainConcertPage.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { NavBar } from "@/components/layouts";
 import { Filters } from "@/components/common";
 import { ConcertListSection } from "@/components/mainConcert";
+import getTodayStringDate from "@/utils/getTodayStringDate";
+import { ShowFilterRequestType } from "@/types";
 
 const MainConcertPage = () => {
+  const { todayString } = getTodayStringDate();
+
+  const [filterRequest, setFilterRequest] = useState<ShowFilterRequestType>({
+    start_date: todayString,
+    end_date: todayString,
+    location: "all",
+    progress: "all",
+    category: "all",
+  });
+
   return (
     <>
       <NavBar />
       <main>
-        <Filters />
-        <ConcertListSection />
+        <Filters filterRequest={filterRequest} setFilterRequest={setFilterRequest} />
+        <ConcertListSection filterRequest={filterRequest} />
       </main>
     </>
   );
