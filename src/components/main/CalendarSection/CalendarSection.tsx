4bc5bfbf7d0c78ee501c0e9edb0ceb0fba7b79d9
ShinEun9 <ses2201@naver.com>
2023-12-14 10:37:55 +0900

    Feat: 버튼 누를 때마다 필터된 공연/전시 정보를 갖고오는 기능 구현 (#2)

--- src/components/main/CalendarSection/CalendarSection.tsx
+++ src/components/main/CalendarSection/CalendarSection.tsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import { DateButton } from "@/components/main";
 import { BasicCard, Button, FilterButton } from "@/components/common";
 import getStringDate from "@/utils/getStringDate";
 import getTodayStringDate from "@/utils/getTodayStringDate";
+import { getShows } from "@/apis";
+import { ShowType } from "@/types/";
 import classNames from "classnames/bind";
 import styles from "./CalendarSection.module.css";
 
 const cx = classNames.bind(styles);
 
 const tabsInfo = [
-  { url: "/concert", value: "공연" },
-  { url: "/exhibition", value: "전시" },
-  { url: "/sports", value: "스포츠" },
+  { url: "concert", value: "공연" },
+  { url: "exhibition", value: "전시" },
+  { url: "sports", value: "스포츠" },
 ];
 
-const Calendar = () => {
+const CalendarSection = () => {
   const navigate = useNavigate();
   const { todayYear, todayMonth, todayDay, todayString } = getTodayStringDate();
-  const [selectedTab, setSelectedTab] = useState("/concert");
+  const [selectedTab, setSelectedTab] = useState("concert");
   const [selectedDate, setSelectedDate] = useState(todayString);
+
+  const { status, data, error } = useQuery({
+    queryKey: ["showDatas", selectedTab, selectedDate],
+    queryFn: async () => await getShows(selectedTab, selectedDate, selectedDate),
+    select: (item) => item.slice(0, 7),
+  });
 
   const handleClickTab = (value: string) => () => {
     setSelectedTab(value);
@@ -27,6 +36,8 @@ const Calendar = () => {
   const handleClickDateBtn = (value: string) => () => {
     setSelectedDate(value);
   };
+
+  if (status === "error") return <div>{error.message}</div>;
 
   return (
     <section className={styles.section}>
@@ -57,21 +68,10 @@ const Calendar = () => {
         ))}
       </div>
       <div className={styles["cards-container"]}>
-        {Array(10)
-          .fill(0)
-          .map((_item, index) => {
-            return (
-              <BasicCard
-                key={index}
-                item={{
-                  id: "1",
-                  imgSrc: "/public/card-image.png",
-                  title: "서울대 전시회에 놀러오세요",
-                  location: "고려대학교 박물관 지하 1층 (백주년기념 삼성관) 기획 전시실 Ⅱ",
-                  date: "2023.11.23 - 2023.11.29",
-                }}
-              />
-            );
+        {status === "pending" && <>Loading...</>}
+        {status !== "pending" &&
+          data.map((item: ShowType) => {
+            return <BasicCard key={item.id} item={item} />;
           })}
       </div>
       <Button
@@ -86,4 +86,4 @@ const Calendar = () => {
   );
 };
 
-export default Calendar;
+export default CalendarSection;
