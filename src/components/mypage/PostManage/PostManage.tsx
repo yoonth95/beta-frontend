4d11cd3764f2b7711c22c4126739c6d4006a6686
ebokyung <dlqhrud303@naver.com>
2023-12-14 16:08:49 +0900

    Remove: PostManage 컴포넌트에 안쓰는 import 제거

--- src/components/mypage/PostManage/PostManage.tsx
+++ src/components/mypage/PostManage/PostManage.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button, Modal } from "@/components/common";
 import { useModalStore } from "@/stores/useModalStore";
 import CommentIcon from "@/assets/comment.svg?react";
