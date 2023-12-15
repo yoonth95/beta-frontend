3212ed84f1202efcbf11b8740cbf88f45150b780
ebokyung <dlqhrud303@naver.com>
2023-12-15 15:29:24 +0900

    Fix: Detail 페이지 getShowInfo 변경된 api url 적용 (#1)

--- src/apis/getShowInfo.tsx
+++ src/apis/getShowInfo.tsx
@@ -2,8 +2,7 @@ import { ShowResponseType } from "@/types";
 import axios from "axios";
 
 export const getShowInfo = async (showId: string) => {
-  // TODO: API URL concert, exhibition ?
   // TODO: try catch
-  const { data } = await axios.get<ShowResponseType>(`/api/concert/${showId}`);
+  const { data } = await axios.get<ShowResponseType>(`/api/detail/${showId}`);
   return data.data[0];
 };
