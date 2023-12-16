26d1a17ff0a448ab5b0c9b31fc191bf237583306
윤태현 <yoonth0919@gmail.com>
2023-12-17 04:04:50 +0900

    Refactor: 회원가입 util 함수 index 설정 (#9)

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,7 @@
 import getShows from "./getShows";
 import getStories from "./getStories";
 import getBannerImages from "./getBannerImages";
+import getUserInfo from "./getUserInfo";
+import postSignupAPI from "./postSignupAPI";
 
-export { getShows, getStories, getBannerImages };
+export { getShows, getStories, getBannerImages, getUserInfo, postSignupAPI };
