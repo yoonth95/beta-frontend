1e2c14d9b59e8f39bbd51914b3bd25fc14777484
ebokyung <dlqhrud303@naver.com>
2023-12-17 23:45:21 +0900

    Design: Editor 컴포넌트 스타일 커스텀

--- src/components/common/Editor/Editor.tsx
+++ src/components/common/Editor/Editor.tsx
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
+import Link from "@ckeditor/ckeditor5-link/src/link";
+import List from "@ckeditor/ckeditor5-list/src/list";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline";
+import "./Editor.css";
 
-const Editor = () => {
-  const [editorData, setEditorData] = useState("");
+const editorConfig = {
+  plugins: [Essentials, Heading, Paragraph, Bold, Italic, Underline, Link, List, BlockQuote],
+  toolbar: ["heading", "|", "bold", "italic", "underline", "link", "|", "bulletedList", "numberedList", "|", "undo", "redo"],
+  heading: {
+    options: [
+      { model: "paragraph", title: "본문", class: "ck-heading_paragraph" },
+      { model: "heading4", view: "h4", title: "제목1", class: "ck-heading_heading1" },
+      { model: "heading5", view: "h5", title: "제목2", class: "ck-heading_heading2" },
+      { model: "heading6", view: "h6", title: "제목3", class: "ck-heading_heading3" },
+    ],
+  },
+};
 
+interface PropsType {
+  editorData: string;
+  setEditorData: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Editor: React.FC<PropsType> = ({ editorData, setEditorData }) => {
   const handleEditorChange = (event, editor) => {
     const data = editor.getData();
     setEditorData(data);
   };
-
-  return <CKEditor editor={ClassicEditor} data={editorData} onChange={handleEditorChange} />;
+  return <CKEditor editor={ClassicEditor} config={editorConfig} data={editorData} onBlur={handleEditorChange} />;
 };
 
 export default Editor;
