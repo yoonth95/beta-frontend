c8a4b0348a45a771c450b56058f5fdbf3b475fe6
ShinEun9 <ses2201@naver.com>
2023-12-17 12:54:26 +0900

    Add: StoryType에 story_color 추가

--- src/types/storyType.ts
+++ src/types/storyType.ts
@@ -7,6 +7,7 @@ export interface StoryType {
   id: number;
   user_id: number;
   story_image_url: string;
+  story_color: string | null;
   tags: string;
   created_at: string;
   updated_at: string;
