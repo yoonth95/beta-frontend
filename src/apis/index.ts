be25cbf1610f3e7303adcc83b6e495d3da79930b
ShinEun9 <ses2201@naver.com>
2023-12-17 12:58:13 +0900

    Feat: 스토리 업로드api 요청 함수 구현 (#2)

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,6 @@
 import getShows from "./getShows";
 import getStories from "./getStories";
 import getBannerImages from "./getBannerImages";
+import postStory from "./postStory";
 
-export { getShows, getStories, getBannerImages };
+export { getShows, getStories, getBannerImages, postStory };
