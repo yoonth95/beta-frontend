98437b5838d082eaba854b067501da6a95a81829
ebokyung <dlqhrud303@naver.com>
2023-12-17 23:44:50 +0900

    Chore: ckeditor5관련 plugin 설치 및 vite.config 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), ckeditor5({ theme: require.resolve("@ckeditor/ckeditor5-theme-lark") })],
   server: {
     port: 3000,
     proxy: {
