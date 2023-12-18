27565bb311304a105c24dd41bfadcda42985d788
ShinEun9 <ses2201@naver.com>
2023-12-18 16:57:34 +0900

    Feat: 상세페이지 댓글 업로드 기능 구현 (#1)

--- src/components/detail/ReviewForm/ReviewForm.tsx
+++ src/components/detail/ReviewForm/ReviewForm.tsx
@@ -1,19 +1,48 @@
-import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { queryClient } from "@/main";
+import { useMutation } from "@tanstack/react-query";
 import { Button } from "@/components/common";
+import { ReviewPostParamType } from "@/types";
+import { useLoginStore } from "@/stores/useLoginStore";
+import { postReview } from "@/apis";
 import styles from "./ReviewForm.module.css";
 
 const ReviewForm = () => {
+  const { id: show_id } = useParams();
+  const {
+    userState: { login_id },
+  } = useLoginStore();
+  const [reviewInput, setReviewInput] = useState("");
+  const { mutate } = useMutation({
+    mutationFn: (comment: ReviewPostParamType) => postReview(comment),
+    onSuccess: () => {
+      setReviewInput("");
+      queryClient.invalidateQueries({
+        queryKey: ["reviewData", show_id],
+      });
+    },
+  });
+
   const handleReviewForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!show_id || !login_id || !reviewInput) return;
+
+    mutate({ show_id, login_id, comment: reviewInput });
   };
+
   return (
-    <form className={styles["review-form"]} onSubmit={handleReviewForm}>
+    <form id={"review-form"} className={styles["review-form"]} onSubmit={handleReviewForm}>
       <textarea
+        value={reviewInput}
+        onChange={(e) => {
+          setReviewInput(e.target.value);
+        }}
         className={styles["review-form__textarea"]}
         autoFocus
         placeholder="방문한 곳은 어떠셨나요? 후기/방명록을 통해 회원님의 멋진 경험을 공유해보세요!"
       />
-      <Button type="submit">
+      <Button type="submit" form={"review-form"}>
         <h3>방명록 등록하기</h3>
       </Button>
     </form>
