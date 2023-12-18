6bec7bd03e3488b7ec84a4b1bac951794800ca21
ShinEun9 <ses2201@naver.com>
2023-12-18 16:54:12 +0900

    Add: 댓글 관련 api export 추가(#1)

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -4,5 +4,9 @@ import getBannerImages from "./getBannerImages";
 import getShowInfo from "./getShowInfo";
 import getShowReservationInfo from "./getShowReservationInfo";
 import getUserInfo from "./getUserInfo";
+import getReviews from "./getReviews";
+import postReview from "./postReview";
+import deleteReview from "./deleteReview";
+import patchReview from "./patchReview";
 
-export { getShows, getStories, getBannerImages, getShowInfo, getShowReservationInfo, getUserInfo };
+export { getShows, getStories, getBannerImages, getShowInfo, getShowReservationInfo, getUserInfo, getReviews, postReview, deleteReview, patchReview };
