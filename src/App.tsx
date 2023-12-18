6eea37b629e78a74b911e79ffaf251c0c529112c
윤태현 <yoonth0919@gmail.com>
2023-12-18 16:09:29 +0900

    Refactor: 라우터마다 로그인 검증 로직 변경 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { Header } from "./components/layouts";
 import PrivateRoute from "./PrivateRoute";
 
 function App() {
   const location = useLocation();
 
-  const isPrivateRoute = location.pathname.startsWith("/mypage");
   return (
     <>
       {location.pathname !== "/signup" && location.pathname !== "/login" && <Header />}
-      {isPrivateRoute ? <PrivateRoute /> : <Outlet />}
+      <PrivateRoute />
     </>
   );
 }
