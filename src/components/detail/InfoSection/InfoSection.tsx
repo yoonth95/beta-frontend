9af5a4e42ba38bcc07a21c104dd971ca0f56d7ac
ebokyung <dlqhrud303@naver.com>
2023-12-19 14:11:51 +0900

    Design: 상세 페이지 infoSection 소개 내용 스타일 추가 (#1)

--- src/components/detail/InfoSection/InfoSection.tsx
+++ src/components/detail/InfoSection/InfoSection.tsx
@@ -24,16 +24,45 @@ const copyClipBoard: onCopyFn = async (text: string) => {
   }
 };
 
+// const decodeBase64UrlSafe = (input: string) => {
+//   // URL-safe base64 decoding
+//   const base64 = input.replace(/-/g, "+").replace(/_/g, "/");
+//   try {
+//     return window.atob(base64);
+//   } catch (error) {
+//     console.error("Error decoding base64:", error);
+//     return "";
+//   }
+// };
+
+const decodeUnicode = (str) => {
+  // try {
+  //   // const decodedBase64 = decodeBase64UrlSafe(str);
+  //   return decodeURIComponent(
+  //     str
+  //       .split("")
+  //       .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+  //       .join(""),
+  //   );
+  // } catch (error) {
+  //   console.error("Error decoding Unicode:", error);
+  //   return "";
+  // }
+  return str;
+};
+
 const InfoSection = () => {
   const { showInfo } = useShowInfoStore();
   if (!showInfo) return <h2>loading...</h2>;
 
   const { univ, department, title, location, start_date, end_date } = showInfo;
 
-  const tags = showInfo.tags && Object.values(JSON.parse(showInfo.tags));
+  const tags = (showInfo.tags && showInfo.tags.length && Object.values(JSON.parse(showInfo.tags))) || [];
   const position = showInfo.position && JSON.parse(showInfo.position);
+
   const contentBufferData = showInfo.content && new Uint8Array(showInfo.content.data);
   const contentDecodedString = contentBufferData && new TextDecoder("utf-8").decode(contentBufferData);
+  const content = contentDecodedString ? decodeUnicode(contentDecodedString) : null;
 
   return (
     <>
@@ -44,20 +73,23 @@ const InfoSection = () => {
           <h4 className={styles["info-title__title"]}>{title}</h4>
           <p className={styles["info-title__date"]}>{start_date + " ~ " + end_date}</p>
           <p className={styles["info-title__location"]}>{location}</p>
-          <ul className={styles["info-title__tags"]}>
-            {tags.map((tag) => (
-              <li key={tag} className={styles.tag}>
-                {tag}
-              </li>
-            ))}
-          </ul>
+          {!tags.length && (
+            <ul className={styles["info-title__tags"]}>
+              {tags.map((tag) => (
+                <li key={tag} className={styles.tag}>
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          )}
         </section>
 
-        <section className={styles["info-description"]}>
-          <h3>소개</h3>
-          {/* 커스텀 글 내용 */}
-          <div>{contentDecodedString}</div>
-        </section>
+        {content && (
+          <section className={styles["info-description"]}>
+            <h3>소개</h3>
+            <pre className={styles["info-description__content"]} dangerouslySetInnerHTML={{ __html: content }}></pre>
+          </section>
+        )}
 
         <section className={styles["info-location"]}>
           <h3 className="a11y-hidden">지도</h3>
