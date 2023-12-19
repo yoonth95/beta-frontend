ac738f2df513a415107ea3a466f99e2cfd2752a9
윤태현 <yoonth0919@gmail.com>
2023-12-19 16:07:20 +0900

    Add: 하루 체크 유틸 함수 추가 (#15)

---
+++ src/utils/isInDay.ts
@@ -0,0 +1,10 @@
+const isInDay = (stringDate: string): boolean => {
+  const inputDate = new Date(stringDate).getTime();
+  const now = new Date().getTime();
+
+  const diffHours = (now - inputDate) / (1000 * 60 * 60);
+
+  return diffHours < 24;
+};
+
+export default isInDay;
