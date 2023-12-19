09e4dd69254cd5bbe7b4da924d15bae4bbf53a13
ebokyung <dlqhrud303@naver.com>
2023-12-19 12:19:31 +0900

    Refactor: LocationMap react-kakao-maps-sdk 라이브러리에서 기본 kakaomap API로 변경 (#1)

--- src/components/detail/InfoSection/LocationMap.tsx
+++ src/components/detail/InfoSection/LocationMap.tsx
@@ -1,51 +1,46 @@
-import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
+import { useEffect, useState } from "react";
 import mapPinIcon from "@/assets/icon-map-pin.svg";
 
-const apiKey = import.meta.env.VITE_APP_KAKAOMAP_API_KEY as string;
+const { kakao } = window;
 
 interface PropsType {
   lat: number;
   lng: number;
 }
 
-export default function LocationMap({ lat, lng }: PropsType) {
-  useKakaoLoader({ appkey: apiKey });
+export default function LocationMap({ lat = 127.055596615858, lng = 37.5069494959122 }: PropsType) {
+  // const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new kakao.maps.LatLng(lat, lng),
+      level: 4, // 지도의 확대 레벨
+    };
+    const map = new kakao.maps.Map(container, options);
+    // setMap(kakaoMap);
+
+    const imageSrc = mapPinIcon; // 마커이미지의 주소입니다
+    const imageSize = new kakao.maps.Size(29, 42); // 마커이미지의 크기입니다
+    const imageOption = { offset: new kakao.maps.Point(14.5, 42) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+    const marker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(lat, lng),
+      image: markerImage,
+    });
+    marker.setMap(map);
+  }, []);
 
   return (
-    <Map // 지도를 표시할 Container
+    <div
       id="map"
-      center={{
-        // 지도의 중심좌표
-        lat,
-        lng,
-      }}
       style={{
-        // 지도의 크기
         width: "100%",
         height: "400px",
       }}
-      level={4} // 지도의 확대 레벨
-    >
-      <MapMarker // 마커를 생성합니다
-        position={{
-          // 마커가 표시될 위치입니다
-          lat,
-          lng,
-        }}
-        image={{
-          src: mapPinIcon, // 마커이미지의 주소입니다
-          size: {
-            width: 29,
-            height: 42,
-          }, // 마커이미지의 크기입니다
-          options: {
-            offset: {
-              x: 14.5,
-              y: 42,
-            }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-          },
-        }}
-      />
-    </Map>
+    ></div>
   );
 }
