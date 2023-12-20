2ad7fabbe1c3b68ce1f930cb6853119991b17c50
ShinEun9 <ses2201@naver.com>
2023-12-20 12:50:46 +0900

    Move: RervationFormModal에서 ReservationForm 컴포넌트화 (#61)

--- src/components/detail/ReservationForm/ReservationForm.tsx
+++ src/components/detail/ReservationForm/ReservationForm.tsx
@@ -1,56 +1,47 @@
-import { useQuery } from "@tanstack/react-query";
 import { Button, CheckBox, InputField, InputFieldGroup, RadioButtonGroup } from "@/components/common";
 import useInputs from "@/hooks/useInputs";
-import { getUserInfo } from "@/apis";
-import { UserReservationFormType, ShowReservationInfoType, UserReservationInputsType } from "@/types";
-import styles from "./ReservationFormModal.module.css";
-
-// TODO: show_times API 연결
-const show_times_data = ["2023/12/08 - 오후 1시", "2023/12/08 - 오후 7시", "2023/12/10 - 오후 1시", "2023/12/10 - 오후 7시"];
+import { UserReservationFormType, UserReservationInputsType, AgencyReservationInfoType, MemberType } from "@/types";
+import styles from "./ReservationForm.module.css";
 
 interface PropsType {
-  showReservationInfo: Omit<ShowReservationInfoType, "method" | "google_form_url">;
+  showInfo: AgencyReservationInfoType;
+  userInfo: MemberType;
+  goToPaymentStep: () => void;
 }
 
-const ReservationFormModal: React.FC<PropsType> = ({ showReservationInfo }) => {
+const ReservationForm: React.FC<PropsType> = ({ showInfo, userInfo, goToPaymentStep }) => {
   const [form, onChange] = useInputs<UserReservationInputsType>({
     show_times_id: 0,
     is_receive_email: false,
   });
 
-  const {
-    status,
-    error,
-    data: userInfo,
-  } = useQuery({
-    queryKey: ["userInfo"],
-    queryFn: () => getUserInfo("yoonth0919"), // TODO: 로그인 정보에서 아이디 값 가져오기
-  });
+  const { location, price, notice, date_time } = showInfo;
+  const show_date_time = date_time.map((item) => item.date_time);
 
-  if (status === "pending") return <h1>loading...</h1>;
-  if (status === "error") return <h1>{error.message}</h1>;
-
-  const { location, price, notice } = showReservationInfo;
   const { user_name, user_email, phone_number } = userInfo;
-
   const [email1, email2] = user_email.split("@");
   const [phone1, phone2, phone3] = phone_number.split("-");
-  const noticeBufferData = new Uint8Array(notice.data);
-  const noticeDecodedString = new TextDecoder("utf-8").decode(noticeBufferData);
+  const noticeBufferData = notice && new Uint8Array(notice.data);
+  const noticeDecodedString = noticeBufferData && new TextDecoder("utf-8").decode(noticeBufferData);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const result: UserReservationFormType = {
       user_id: userInfo.id,
-      show_id: showReservationInfo.show_id,
+      show_id: showInfo.show_id,
       show_times_id: form.show_times_id,
       is_receive_email: form.is_receive_email ? 1 : 0,
     };
-    console.log(result);
+    if (price === 0) {
+      //  post 요청
+      return;
+    }
+
+    goToPaymentStep();
   };
 
   return (
-    <section className={styles["reservation-section"]}>
+    <>
       <div className={styles["show-info"]}>
         <h2>공연 정보</h2>
         <p>
@@ -71,7 +62,7 @@ const ReservationFormModal: React.FC<PropsType> = ({ showReservationInfo }) => {
       <form id="reservation" onSubmit={handleSubmit}>
         <div className={styles["show-round"]}>
           <h2>회차 선택</h2>
-          <RadioButtonGroup radioList={show_times_data} name="round" onChange={onChange} flexDirectionColumn />
+          <RadioButtonGroup radioList={show_date_time} name="round" onChange={onChange} flexDirectionColumn />
         </div>
 
         <div className={styles["show-reservation-user-info"]}>
@@ -88,8 +79,20 @@ const ReservationFormModal: React.FC<PropsType> = ({ showReservationInfo }) => {
         </div>
       </form>
 
-      {/* TODO: show_sub_type일때  */}
-      {/* {show_sub_type === "연극" && (
+      <Button type="submit" form="reservation">
+        예매하기
+      </Button>
+    </>
+  );
+};
+
+export default ReservationForm;
+
+{
+  /* TODO: show_sub_type일때  */
+}
+{
+  /* {show_sub_type === "연극" && (
         <div className={styles["show-agreement"]}>
           <h2 className="a11y-hidden">동의서</h2>
           <CheckBox inputId="외부 유출 금지 동의">
@@ -102,13 +105,5 @@ const ReservationFormModal: React.FC<PropsType> = ({ showReservationInfo }) => {
             <span>*노쇼 (NO SHOW)및 무단양도 관객의 경우 다음 oo대학교 oo학과 공연 예매가 제한 될 수 있음을 안내드립니다.</span>
           </CheckBox>
         </div>
-      )} */}
-
-      <Button type="submit" form="reservation">
-        예매하기
-      </Button>
-    </section>
-  );
-};
-
-export default ReservationFormModal;
+      )} */
+}
