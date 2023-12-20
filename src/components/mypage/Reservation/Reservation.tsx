80e4fc6c05aaf1f0fa29cbfa89a4c93b534b4bda
윤태현 <yoonth0919@gmail.com>
2023-12-20 22:56:22 +0900

    Design: 마이페이지 유저 예매 관리 마크업 및 디자인 (#15)

--- src/components/mypage/Reservation/Reservation.tsx
+++ src/components/mypage/Reservation/Reservation.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames/bind";
+import styles from "./Reservation.module.css";
+
+const cx = classNames.bind(styles);
 
 const Reservation = () => {
-  return <div>예매내역</div>;
+  return (
+    <div className={styles["reservation-container"]}>
+      <div className={styles["reservation-box"]}>
+        <img className={styles["reservation-img"]} src="" alt="" />
+        <div className={styles["reservation-item"]}>
+          <div>
+            <h3>제목 : </h3>
+            <p>장소</p>
+            <p>날짜</p>
+          </div>
+          <button className={styles["reservation-delete-button"]}>예매 정보</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Reservation;
