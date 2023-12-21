840f8c0155d8ec6aa7df2943d299f66dbd05969c
ShinEun9 <ses2201@naver.com>
2023-12-21 10:00:41 +0900

    Feat: 결제 실패시 뒤로가기 버튼 구현 (#61)

--- src/pages/PayFail/PayFailPage.tsx
+++ src/pages/PayFail/PayFailPage.tsx
@@ -1,6 +1,8 @@
+import { useReservationFormStore } from "@/stores/useReservationFormStore";
 import { Link, useSearchParams } from "react-router-dom";
 
 const PayFailPage = () => {
+  const { reservationForm } = useReservationFormStore();
   const [searchParams] = useSearchParams();
   const errorCode = searchParams.get("code");
   const errorMessage = searchParams.get("message");
@@ -27,8 +29,8 @@ const PayFailPage = () => {
 
         <div className="w-100 button-group">
           <div className="flex" style={{ gap: "16px" }}>
-            <Link to="/" className="btn w-100">
-              홈으로
+            <Link to={`/detail/${reservationForm?.show_id}`} className={"btn w-100"}>
+              뒤로 가기
             </Link>
             <Link to="/" className={"btn w-100 red"}>
               홈으로 이동
