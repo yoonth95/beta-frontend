42df48c84b150bd94009820868cc3d5f417e767c
ShinEun9 <ses2201@naver.com>
2023-12-21 09:59:07 +0900

    Feat: 토스페이먼츠 결제 후 예매 api 요청 함수 구현 (#61)

---
+++ src/apis/postReservation.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+interface PostReservationParamType {
+  orderId?: string;
+  amount?: string;
+  show_id: number;
+  show_times_id: number;
+  is_receive_email: 1 | 0;
+}
+
+const postReservation = async (data: PostReservationParamType) => {
+  try {
+    await axios.post("/api/confirm", data);
+  } catch (err) {
+    throw "서버 요청 실패";
+  }
+};
+
+export default postReservation;
