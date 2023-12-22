3ea26fffe4171efff8f3afbe4f74f3ea9428b6c0
ShinEun9 <ses2201@naver.com>
2023-12-22 10:44:33 +0900

    Design: GuestAccess 컴포넌트에 로그인한 사람이 관리자일때 마크업 추가(#4)

--- src/components/detail/GuestAccess/GuestAccess.tsx
+++ src/components/detail/GuestAccess/GuestAccess.tsx
@@ -1,19 +1,40 @@
-import React from "react";
-import styles from "./GuestAccess.module.css";
 import { Link } from "react-router-dom";
+import { useLoginStore } from "@/stores/useLoginStore";
+import styles from "./GuestAccess.module.css";
 
 const GuestAccess = () => {
+  const {
+    userState: { user_role },
+  } = useLoginStore();
+
   return (
-    <article className={styles["guest-access"]}>
-      <div className={styles["guest-access__signup"]}>
-        <p>아직 회원이 아니시라면,</p>
-        <Link to="/signup">회원가입</Link>
-      </div>
-      <div className={styles["guest-access__login"]}>
-        <p>이미 가입한 아이디가 있다면,</p>
-        <Link to="/login">로그인</Link>
-      </div>
-    </article>
+    <div className={styles["guest-access"]}>
+      {user_role === "" && (
+        <>
+          <div className={styles["guest-access__signup"]}>
+            <p>아직 회원이 아니시라면,</p>
+            <Link to="/signup">회원가입</Link>
+          </div>
+          <div className={styles["guest-access__login"]}>
+            <p>이미 가입한 아이디가 있다면,</p>
+            <Link to="/login">로그인</Link>
+          </div>
+        </>
+      )}
+
+      {user_role === "admin" && (
+        <>
+          <div className={styles["guest-access__signup"]}>
+            <p>일반 회원만 사용할 수 있는 기능입니다.</p>
+            <Link to="/signup">일반 회원가입</Link>
+          </div>
+          <div className={styles["guest-access__login"]}>
+            <p>이미 가입한 아이디가 있다면,</p>
+            <Link to="/login">로그인</Link>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
