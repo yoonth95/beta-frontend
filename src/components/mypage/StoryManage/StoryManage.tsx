f39246bee04a9f2e444eca9dfa5c4648f6cfc4c8
윤태현 <yoonth0919@gmail.com>
2023-12-23 15:46:50 +0900

    Refactor: 마이페이지 스토리 관리 값이 있을 때와 없을 때 분리 (#45)

--- src/components/mypage/StoryManage/StoryManage.tsx
+++ src/components/mypage/StoryManage/StoryManage.tsx
@@ -18,24 +18,26 @@ const StoryManage = () => {
   if (status === "error") return <h1>{error.message}</h1>;
 
   return (
-    <div className={styles["story-container"]}>
+    <>
       {userStoryList.length > 0 ? (
-        userStoryList.map((item) => (
-          <StoryItem
-            key={item.id}
-            id={item.id}
-            login_id={item.login_id}
-            story_image_url={item.story_image_url}
-            story_color={item.story_color}
-            tags={item.tags}
-            created_at={item.created_at}
-            updated_at={item.updated_at}
-          />
-        ))
+        <div className={styles["story-container"]}>
+          {userStoryList.map((item) => (
+            <StoryItem
+              key={item.id}
+              id={item.id}
+              login_id={item.login_id}
+              story_image_url={item.story_image_url}
+              story_color={item.story_color}
+              tags={item.tags}
+              created_at={item.created_at}
+              updated_at={item.updated_at}
+            />
+          ))}
+        </div>
       ) : (
         <NullField text1="작성한 스토리가 없어요!" text2="좋은 추억을 남겨보세요!" />
       )}
-    </div>
+    </>
   );
 };
 
