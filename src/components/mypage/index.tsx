79b983b8fd1e0dfc32d1c8da8644999f4c6befb9
윤태현 <yoonth0919@gmail.com>
2023-12-24 21:00:45 +0900

    Refactor: 마이페이지 유저 예매 관리 컴포넌트 분리 (#45)

--- src/components/mypage/index.tsx
+++ src/components/mypage/index.tsx
@@ -1,7 +1,9 @@
 import Postcode from "./admin/Postcode/Postcode";
 import ReservationForm from "./admin/ReservationForm/ReservationForm";
 import ReservationListModal from "./admin/ReservationListModal/ReservationListModal";
+import ReservationItem from "./user/ReservationItem/ReservationItem";
+import ReservationUserModal from "./user/ReservationUserModal/ReservationUserModal";
 import ReviewItem from "./user/ReviewItem/ReviewItem";
 import StoryItem from "./user/StoryItem/StoryItem";
 
-export { ReservationListModal, ReservationForm, Postcode, ReviewItem, StoryItem };
+export { ReservationListModal, ReservationForm, Postcode, ReviewItem, StoryItem, ReservationItem, ReservationUserModal };
