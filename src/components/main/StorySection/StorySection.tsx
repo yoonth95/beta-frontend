4892aa17eaed509955e464c6f47e7df172408ccb
ebokyung <dlqhrud303@naver.com>
2023-12-24 23:18:19 +0900

    Design: 스토리 Section, Card 스타일 수정 (#2)

--- src/components/main/StorySection/StorySection.tsx
+++ src/components/main/StorySection/StorySection.tsx
@@ -74,12 +74,12 @@ const StorySection = () => {
         {openModal.state && (
           <>
             {openModal.type === "upload" && (
-              <Modal width={"18.75rem"} height={"36.25rem"} title={"스토리 업로드"}>
+              <Modal width={"25rem"} height={"41rem"} title={"스토리 업로드"}>
                 <StoryUploadModal />
               </Modal>
             )}
             {openModal.type === "more" && (
-              <Modal title={"스토리"} titleHidden={true}>
+              <Modal height={"100vh"} title={"스토리"} titleHidden>
                 <StoryViewModal initialSlide={initialSlide} />
               </Modal>
             )}
