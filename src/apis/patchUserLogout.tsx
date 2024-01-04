3ec7b580238886c91eec22cdec8b87e1d440a22a
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-01-04 18:09:09 +0900

    Update patchUserLogout.tsx

--- src/apis/patchUserLogout.tsx
+++ src/apis/patchUserLogout.tsx
@@ -6,6 +6,6 @@ interface PropsTpye {
 }
 
 export const patchUserLogout = async (): Promise<PropsTpye> => {
-  const { data } = await axios.patch(`${import.meta.env.VITE_APP_API_ENDPOINT}/api/logout`);
+  const { data } = await axios.patch(`${import.meta.env.VITE_APP_API_ENDPOINT}/api/logout`, { withCredentials: true });
   return data;
 };
